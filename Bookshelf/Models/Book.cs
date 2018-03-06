ae2605244d59cd46c01820ce78df6db61f3a3798
bethbenjamin <bethbenjamin@gmail.com>
2018-03-06 10:20:36 -0500

    classes added

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,13 @@ namespace Bookshelf.Models
     {
         [Key]
         public int BookID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+    }
+}
