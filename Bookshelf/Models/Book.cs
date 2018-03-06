3bfd284dbb60ea99b5261eab6f4c25b0ce5b89a6
bethbenjamin <bethbenjamin@gmail.com>
2018-03-06 10:01:12 -0500

    first commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+    }
+}
